d89c366c216d5f946e828122862071db23bfc5f5
lawrenceAigo <lawrenceaigo@163.com>
2022-02-15 16:05:14 +0800

    test

--- Solidity-version-contract-for-Aurora/migrations/2_intial_airdropfactory.js
+++ Solidity-version-contract-for-Aurora/migrations/2_intial_airdropfactory.js
@@ -2,8 +2,8 @@ const AirDropFactoryOrchestrator = artifacts.require("AirDropFactoryOrchestrator
 
 module.exports = function (deployer) {
     let airDropFactoryOrchestrator;
-  deployer.deploy(AirDropFactoryOrchestrator);
-   deployer.then(function() {
+    deployer.deploy(AirDropFactoryOrchestrator);
+    deployer.then(function() {
 	   
 	  	return AirDropFactoryOrchestrator.deployed();
 	}).then(function(instance) {
